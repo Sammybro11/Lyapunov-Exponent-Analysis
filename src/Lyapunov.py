30ffa08743eb8adb71ba1c76f37d288d0fd0e349
Samyak <73867967+Sammybro11@users.noreply.github.com>
2025-11-01 18:08:54 +0530

    Update Lyapunov.py

--- src/Lyapunov.py
+++ src/Lyapunov.py
@@ -3,13 +3,14 @@ from src.Model_Structures import DoublePendulum, Create_Henon_Map
 from src.Helper_Functions import rk4_step, Gram_Schmidt_Orthonormalization
 from tqdm import tqdm
 
+EPS = 1e-10
+
 def Lyapunov_Exponent_Henon(a, b):
     Map = Create_Henon_Map(a,b)
 
     init_coordinates = np.array([0.2, 0.2])
 
     perturbation_vectors = np.eye(2, dtype=float)
-    # Each Column is an ortho unit vector in the 2 x 2 id matrix
 
     sum_log = 0
     transient = 3000
@@ -33,7 +34,7 @@ def Lyapunov_Exponent_Henon(a, b):
         Q, R = Gram_Schmidt_Orthonormalization(perturbation)
 
         diag = np.abs(np.diag(R)) # Norm of each vector before normalization, the "stretch"
-        diag[diag < 1e-14] = 1e-14 # Incase vector becomes tiny
+        diag[diag < EPS] = EPS
         sum_log += np.log(diag)
 
         perturbation_vectors = Q
